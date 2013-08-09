dc86bec77e0a26025d1b24b47d3ca48799d2558f
Alexander <a.yelfutin@gmail.com>
2013-08-10 00:55:40 +0300

    final

--- js/master.js
+++ js/master.js
@@ -1,5 +1,26 @@
 /* карты */
 !function($) {
 	"use strict"; // jshint ;_;
-}(window.jQuery);
 
+	$("select").selectBox();
+	
+	$('[data-toggle="tab"]').on('click',function(e){
+		$('.nav_wrapp li.active, .tabs-wrap>.active').removeClass('active');
+		$(this).parent().addClass('active');
+		$($(this).attr('href')).addClass('active');
+		
+		return false;
+	})
+	
+	$(".gSet, .setH").on('click',function(e){
+		$('.setH').fadeToggle();
+	})
+
+	
+	$(".payment-type").on('click','.btn',function(e){
+		$('.payment-type .btn.active').removeClass('active');
+		$('.payment-details>.active').removeClass('active');
+		$(this).addClass('active');
+		$('#'+$(this).data("detail")).addClass('active');
+	})
+}(window.jQuery);

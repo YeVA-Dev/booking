fc0baa2b683a8d5337defac90f2f0e0e820cbf10
Alexander <a.yelfutin@gmail.com>
2013-08-11 16:26:50 +0300

    2013-08-11

--- js/master.js
+++ js/master.js
@@ -3,24 +3,51 @@
 	"use strict"; // jshint ;_;
 
 	$("select").selectBox();
-	
-	$('[data-toggle="tab"]').on('click',function(e){
+
+	$('[data-toggle="tab"]').on('click', function(e) {
 		$('.nav_wrapp li.active, .tabs-wrap>.active').removeClass('active');
 		$(this).parent().addClass('active');
 		$($(this).attr('href')).addClass('active');
-		
+
 		return false;
 	})
-	
-	$(".gSet, .setH").on('click',function(e){
+
+	$(".gSet, .setH").on('click', function(e) {
 		$('.setH').fadeToggle();
 	})
 
-	
-	$(".payment-type").on('click','.btn',function(e){
+	$(".payment-type").on('click', '.btn', function(e) {
 		$('.payment-type .btn.active').removeClass('active');
 		$('.payment-details>.active').removeClass('active');
 		$(this).addClass('active');
-		$('#'+$(this).data("detail")).addClass('active');
+		$('#' + $(this).data("detail")).addClass('active');
 	})
+
+	$(".control-group .controls:last-child, .easy-list li:last-child").addClass("last")
+
+	jQuery.support.placeholder = false;
+	var test = document.createElement('input');
+	if ('placeholder' in test)
+		jQuery.support.placeholder = true;
+
+	if (!$.support.placeholder) {
+		var active = document.activeElement;
+		$(':text').focus(function() {
+			if ($(this).attr('placeholder') != '' && $(this).val() == $(this).attr('placeholder')) {
+				$(this).val('').removeClass('hasPlaceholder');
+			}
+		}).blur(function() {
+			if ($(this).attr('placeholder') != '' && ($(this).val() == '' || $(this).val() == $(this).attr('placeholder'))) {
+				$(this).val($(this).attr('placeholder')).addClass('hasPlaceholder');
+			}
+		});
+		$(':text').blur();
+		$(active).focus();
+		$('form').submit(function() {
+			$(this).find('.hasPlaceholder').each(function() {
+				$(this).val('');
+			});
+		});
+	}
+	
 }(window.jQuery);
